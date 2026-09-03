5429a5dd90d03322e6541a8c85e9e6d2b46e707f
kovemu <kovemusin@gmail.com>
2026-09-03 18:53:36 +0900

    refactor: mirror reference claw state machine

--- Assets/_Game/Machine/MachineState.cs
+++ Assets/_Game/Machine/MachineState.cs
@@ -2,11 +2,13 @@ namespace Claw3D.Machine
 {
     public enum MachineState
     {
+        Idle,
         Aim,
         Drop,
         Grip,
         Lift,
         Return,
-        Release
+        Release,
+        Score
     }
 }
