7fc748f9b72fe16075f63a8386310f68a909fbb6
kovemu <kovemusin@gmail.com>
2026-09-05 21:14:56 +0900

    feat: add source-shaped ClawReferences

---
+++ ReferenceCloneProject/Assets/Scripts/Claw/ClawReferences.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Claw
+{
+    public sealed class ClawReferences : MonoBehaviour
+    {
+        public List<ClawArm> claws = new List<ClawArm>();
+    }
+}
